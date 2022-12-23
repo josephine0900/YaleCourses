bdd481aab94392f40702c71ef9bb4f6d638a67a9
josephine0900 <76456547+josephine0900@users.noreply.github.com>
2022-12-23 14:40:29 -0500

    Delete Workers.py

--- Workers.py
+++
@@ -1,9 +0,0 @@
-class Worker:
-    """"""
-    def __init__(self, name, player, location):
-        self.name = name
-        self.player = player
-        self.location = location
-
-    def __str__(self):
-        print(self.name)
